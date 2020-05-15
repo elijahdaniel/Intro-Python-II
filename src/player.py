9240159970774aef3c23cf1b97aad33c821b9084
Elijah Paminsan <e.paminsan@gmail.com>
2020-05-14 18:28:48 -0700

    player can navigate to rooms
    working through items portion

--- src/player.py
+++ src/player.py
@@ -1,2 +1,22 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, player_name, current_room):
+        self.player_name = player_name
+        self.current_room = current_room
+
+    def move_to(self, movement, current_location):
+        # move in specified direction
+        attribute = movement + '_to'
+
+        # if current_loc has attribute, you can move in that direction
+        if hasattr(current_location, attribute):
+            # if it is, fetch room in specified direction
+            return getattr(current_location, attribute)
+
+        # Print an error message if the movement isn't allowed.
+        print('\nYou cant go that way!!')
+
+        return current_location
--- src/room.py
+++ src/room.py
@@ -1,2 +1,16 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    def __init__(self, room_name, description):
+        self.room_name = room_name
+        self.description = description
+
+    def __str__(self):
+        # prints current room
+        return f'{self.room_name}'
+
+    def print_description(self):
+        # prints room description
+        return f'{self.description}'
